93d696d3fee6040009c171363a8f8bcb00cc0c7e
yujin_tan <yujinost@gmail.com>
2025-02-28 16:05:32 +0000

    inserting styles into the template component banner

--- src/components/banner.tsx
+++ src/components/banner.tsx
@@ -1,12 +1,12 @@
 import globe from "../../public/globe.svg";
 import Image from "next/image";
-import  banner from './banner.module.css';
+import styles from './banner.module.css';
 
 const Banner = () => {
     return (
         <header className="row">
             <div className="col-5">
-                <Image src={globe} alt="logo" className={banner.logo} />
+                <Image src={globe} alt="logo" className={styles.logo} />
             </div>
             <div className="col-7">
                 Providing houses
