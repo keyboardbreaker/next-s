442dd13be71a53b29bd11e3887058176f886c5cb
yujin_tan <yujinost@gmail.com>
2025-03-13 17:30:03 +0000

    conditional styling

--- src/components/houseRow.tsx
+++ src/components/houseRow.tsx
@@ -11,7 +11,7 @@ const HouseRow = ({ house } : { house: House }) => {
         <tr>
             <td>{house.address}</td>
             <td>{house.country}</td>
-            <td>{currencyFormatter.format(house.price)}</td>
+            <td className={`${house.price > 500000 ? "text-primary": ""}`}>{currencyFormatter.format(house.price)}</td>
         </tr>
     );
 }
