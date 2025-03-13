e7fc07ec0286583b10e156c5f664f7aba9db955a
yujin_tan <yujinost@gmail.com>
2025-03-13 18:18:18 +0000

    app conditionally show house or houseList

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,11 +1,18 @@
+import { useState } from "react";
 import Banner from "./banner";
+import House from "./house";
 import HouseList from "./houseList";
 
 const App = () => {
+    const [selectedHouse, setSelectedHouse] = useState();
     return (
         <>
             <Banner />
             <HouseList />
+            {selectedHouse ? 
+                <House house={selectedHouse} /> : 
+                <HouseList /> 
+            }
         </>
     );
 };
---
+++ src/components/house.tsx
@@ -0,0 +1,27 @@
+// import currencyFormatter from "@/helpers/currencyFormatter";
+import defaultPhoto from "@/helpers/defaultPhoto";
+
+type House = {
+    address: string;
+    country: string;
+    price: number;
+    photo: string;
+};
+
+const House = ({ house }: { house: House }) => {
+    return (
+        <div className="row">
+            <div className="col-6">
+                <div className="row">
+                    <img className="img-fluid"
+                        src={
+                            house.photo ? `./houseImages/${house.photo}.jpeg` : defaultPhoto
+                        }
+                        alt="House Picture" />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default House;
